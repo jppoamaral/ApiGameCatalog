d5288eb6b9dd3d70307d6a8dd32ea0ff4c1fb0c7
JP Amaral <jppoamaral@hotmail.com>
2021-07-26 21:17:12 -0300

    first commit

---
+++ Entities/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGameCatalog.Entities
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public double Price { get; set; }
+    }
+}
