c0d863c817060318b94e986901129d40a5c26e87
JP Amaral <jppoamaral@hotmail.com>
2021-07-26 21:20:03 -0300

    first commit

---
+++ InputModel/GameInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGameCatalog.InputModel
+{
+    public class GameInputModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Game´s Title must contain between 3 and 100 characters")]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Publisher's Name must contain between 3 and 100 characters")]
+        public string Publisher { get; set; }
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Price must have a $1 minimum and a $1000 maximum")]
+        public double Price { get; set; }
+    }
+}
