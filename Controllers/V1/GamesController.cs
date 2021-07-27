6e2221849537e014484daa90718fb32331c59d8e
JP Amaral <jppoamaral@hotmail.com>
2021-07-26 21:17:29 -0300

    first commit

---
+++ Controllers/V1/GamesController.cs
@@ -0,0 +1,103 @@
+﻿using ApiGameCatalog.Exceptions;
+using ApiGameCatalog.InputModel;
+using ApiGameCatalog.Services;
+using ApiGameCatalog.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGameCatalog.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly IGameService _gameService;
+
+        public GamesController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GameViewModel>>> Get([FromQuery, Range(1, int.MaxValue)] int page = 1, [FromQuery, Range(1, 50)] int amount = 5)
+        {
+            var games = await _gameService.Get(page, amount);
+
+            if (games.Count() == 0)
+                return NoContent();
+
+            return Ok(games);
+        }
+
+        [HttpGet("{gameID:guid}")]
+        public async Task<ActionResult<GameViewModel>> Get([FromRoute] Guid gameID)
+        {
+            var game = await _gameService.Get(gameID);
+            if (game == null)
+                return NoContent();
+            return Ok(game);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GameViewModel>> InsertGame([FromBody] GameInputModel gameInputModel)
+        {
+            try
+            {
+                var game = await _gameService.Insert(gameInputModel);
+
+                return Ok(game);
+            }
+            catch(GameRegisteredException ex)
+            {
+                return UnprocessableEntity("There is an existing game with this title made by this publisher");
+            }
+        }
+
+        [HttpPut("{gameID:guid}")]
+        public async Task<ActionResult> UpdateGame([FromRoute] Guid gameID, [FromBody] GameInputModel gameInputModel)
+        {
+            try
+            {
+                await _gameService.Update(gameID, gameInputModel);
+                return Ok();
+            }
+            catch(GameNotRegisteredException ex)
+            {
+                return NotFound("This game does not exist");
+            }
+        }
+
+        [HttpPatch("{gameID:guid}/price/{price:double}")]
+        public async Task<ActionResult> UpdateGame([FromRoute] Guid gameID, [FromRoute] double price)
+        {
+            try
+            {
+                await _gameService.Update(gameID, price);
+                return Ok();
+            }
+            catch (GameNotRegisteredException ex)
+            {
+                return NotFound("This game does not exist");
+            }
+        }
+
+        [HttpDelete("{gameID:guid}")]
+        public async Task<ActionResult> DeleteGame([FromRoute] Guid gameID)
+        {
+            try
+            {
+                await _gameService.Remove(gameID);
+                return Ok();
+            }
+            catch (GameNotRegisteredException ex)
+            {
+                return NotFound("This game does not exist");
+            }
+        }
+    }
+}
---
+++ Controllers/V1/LifeCycleIDController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiGameCatalog.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LifeCycleIDController : ControllerBase
+    {
+        public readonly IExampleSingleton _exSingleton1;
+        public readonly IExampleSingleton _exSingleton2;
+
+        public readonly IExampleScoped _exScoped1;
+        public readonly IExampleScoped _exScoped2;
+
+        public readonly IExampleTransient _exTransient1;
+        public readonly IExampleTransient _exTransient2;
+
+        public LifeCycleIDController(IExampleSingleton exampleSingleton1,
+                                       IExampleSingleton exampleSingleton2,
+                                       IExampleScoped exampleScoped1,
+                                       IExampleScoped exampleScoped2,
+                                       IExampleTransient exampleTransient1,
+                                       IExampleTransient exampleTransient2)
+        {
+            _exSingleton1 = exampleSingleton1;
+            _exSingleton2 = exampleSingleton2;
+            _exScoped1 = exampleScoped1;
+            _exScoped2 = exampleScoped2;
+            _exTransient1 = exampleTransient1;
+            _exTransient2 = exampleTransient2;
+        }
+
+        [HttpGet]
+        public Task<string> Get()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Singleton 1: {_exSingleton1.Id}");
+            stringBuilder.AppendLine($"Singleton 2: {_exSingleton2.Id}");
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine($"Scoped 1: {_exScoped1.Id}");
+            stringBuilder.AppendLine($"Scoped 2: {_exScoped2.Id}");
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine($"Transient 1: {_exTransient1.Id}");
+            stringBuilder.AppendLine($"Transient 2: {_exTransient2.Id}");
+
+            return Task.FromResult(stringBuilder.ToString());
+        }
+
+    }
+
+    public interface IGeneralExample
+    {
+        public Guid Id { get; }
+    }
+
+    public interface IExampleSingleton : IGeneralExample
+    { }
+
+    public interface IExampleScoped : IGeneralExample
+    { }
+
+    public interface IExampleTransient : IGeneralExample
+    { }
+
+    public class LifeCycleExample : IExampleSingleton, IExampleScoped, IExampleTransient
+    {
+        private readonly Guid _guid;
+
+        public LifeCycleExample()
+        {
+            _guid = Guid.NewGuid();
+        }
+
+        public Guid Id => _guid;
+    }
+}
