9a66fec3bf6c46eeadd86209a966419999d99500
JP Amaral <jppoamaral@hotmail.com>
2021-07-26 21:19:05 -0300

    first commit

---
+++ Exceptions/GameNotRegisteredException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGameCatalog.Exceptions
+{
+    public class GameNotRegisteredException : Exception
+    {
+        public GameNotRegisteredException()
+            : base("Game is not registered")
+        {
+        }
+    }
+}
---
+++ Exceptions/GameRegisteredException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGameCatalog.Exceptions
+{
+    public class GameRegisteredException : Exception
+    {
+        public GameRegisteredException()
+            : base("Game was already resgistered")
+        {
+        }
+    }
+}
